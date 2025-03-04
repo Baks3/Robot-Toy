c887574d04e1f1e0ba6a6707f87d39499de94b1a
tmabizela023 <tmabizela023@student.wethinkcode.co.za>
2025-03-04 14:20:58 +0200

    refactored main method/function to meet the SonarQube threshold of 15 by introducing new functions

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import toy
 
+def get_steps(command):
+    while True:
+        try:
+            steps = int(input(f"Enter steps to move {command}: "))
+            if steps > 0:
+                return steps
+            else:
+                print("Please enter a positive number of steps.")
+        except ValueError:
+            print("Invalid input! Please enter a valid number.")
+
+def handle_movement(command):
+    if command in ["forward", "back"]:
+        steps = get_steps(command)
+        toy.move_robot(command, steps)
+    elif command in ["right", "left"]:
+        toy.move_robot(command, 0)
+    else:
+        print("Please enter a valid command!")
+
 def main():
     while True:
         command = input("Enter command (forward, back, right, left, or exit to quit): ").strip().lower()
@@ -8,23 +28,7 @@ def main():
             print("Exiting the robot control program.")
             break
 
-        if command in ["forward", "back"]:
-            while True:
-                try:
-                    steps = int(input(f"Enter steps to move {command}: "))
-                    if steps > 0:
-                        break 
-                    else:
-                        print("Please enter a positive number of steps.")
-                except ValueError:
-                    print("Invalid input! Please enter a valid number.")
-            
-            toy.move_robot(command, steps)
-        elif command in ["right", "left"]:
-            
-            toy.move_robot(command, 0)
-        else:
-            print("Please enter a valid command!")
+        handle_movement(command)
 
 if __name__ == "__main__":
     main()

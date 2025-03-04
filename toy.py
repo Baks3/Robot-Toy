454ea0bcbc4e7b947b4d7d0744220a2e5291656b
tmabizela023 <tmabizela023@student.wethinkcode.co.za>
2025-03-04 13:52:03 +0200

    added a main method to handle user inputs and commands

--- toy.py
+++ toy.py
@@ -44,28 +44,3 @@ def move_robot(command, steps):
     else:
         print("Please enter a valid command!")
 
-
-while True:
-    command = input("Enter command (forward, back, right, left, or exit to quit): ").strip().lower()
-
-    if command == "exit":
-        print("Exiting the robot control program.")
-        break
-
-    if command in ["forward", "back"]:
-        while True:
-            try:
-                steps = int(input(f"Enter steps to move {command}: "))
-                if steps > 0:
-                    break 
-                else:
-                    print("Please enter a positive number of steps.")
-            except ValueError:
-                print("Invalid input! Please enter a valid number.")
-        
-        move_robot(command, steps)
-    elif command in ["right", "left"]:
-        
-        move_robot(command, 0)
-    else:
-        print("Please enter a valid command!")
